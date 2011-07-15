25a700762d09720152aa149430cbab6f65735f06
rimmington <rimmington@gmail.com>
2011-07-16 01:22:28 +1000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gae-sessions',
+    version='1.07',
+    packages=['gaesessions'],
+    author='David Underhill',
+    author_email='dgu@cs.stanford.edu',
+    license='Apache License Version 2.0',
+    url='https://github.com/dound/gae-sessions',
+    description='gae-sessions is a sessions library for the Python runtime on Google App Engine for ALL session sizes. It is extremely fast, lightweight (one file), and easy to use.'
+)
